158e52957a572d28856fba75b22c653346297e43
Nigel van Keulen <nigel2392@gmail.com>
2024-03-28 10:49:44 +0100

    Bump

--- wagtail_editorjs/__init__.py
+++ wagtail_editorjs/__init__.py
@@ -7,5 +7,5 @@ Includes support for custom blocks, inline tools, and more.
 Everything is dynamic and customizable.
 """
 
-__version__ = '1.4.4'
+__version__ = '1.4.5'
 VERSION = tuple([int(v) for v in __version__.split('.')])
