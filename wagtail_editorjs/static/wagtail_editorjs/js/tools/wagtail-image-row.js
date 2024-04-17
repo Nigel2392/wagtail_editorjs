6406e04daa9d4c739742d58af4f3febed744dc7f
Nigel van Keulen <nigel2392@gmail.com>
2024-04-17 21:35:49 +0200

    Update image row tool to allow for choosing different images / removing from row

--- wagtail_editorjs/static/wagtail_editorjs/js/tools/wagtail-image-row.js
+++ wagtail_editorjs/static/wagtail_editorjs/js/tools/wagtail-image-row.js
@@ -11,6 +11,13 @@ const wagtailImageRowToolAddIcon = `<svg xmlns="http://www.w3.org/2000/svg" widt
     <path fill-rule="evenodd" d="M11 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8M6.025 7.5a5 5 0 1 1 0 1H4A1.5 1.5 0 0 1 2.5 10h-1A1.5 1.5 0 0 1 0 8.5v-1A1.5 1.5 0 0 1 1.5 6h1A1.5 1.5 0 0 1 4 7.5zM11 5a.5.5 0 0 1 .5.5v2h2a.5.5 0 0 1 0 1h-2v2a.5.5 0 0 1-1 0v-2h-2a.5.5 0 0 1 0-1h2v-2A.5.5 0 0 1 11 5M1.5 7a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5z"/>
 </svg>`
 
+const wagtailImageRowTrashCanIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+    <!-- The MIT License (MIT) -->
+    <!-- Copyright (c) 2011-2024 The Bootstrap Authors -->
+    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+</svg>`;
+
 
 class ImageRowTool extends window.BaseWagtailEditorJSTool {
     constructor({ data, api, config, block }) {
@@ -92,6 +99,43 @@ class ImageRowTool extends window.BaseWagtailEditorJSTool {
         image.alt = imageData.title;
         image.dataset.imageId = imageData.id;
         image.dataset.editUrl = imageData.edit_url;
+
+        const imageActions = imageWrapper.addElement('div', {
+            className: 'wagtail-image-row-image-actions',
+        });
+
+        const imageEdit = imageActions.addElement('button', {
+            className: 'wagtail-image-row-image-edit',
+            innerHTML: wagtailImageRowToolAddIcon,
+        });
+
+        imageEdit.addEventListener('click', () => {
+            this.imageChooser.openChooserModal(image.dataset.imageId);
+            let changeEventFunc;
+            changeEventFunc = () => {
+                const data = this.imageChooser.getState();
+                image.src = `${this.config.getImageUrl}${data.id}/`;
+                image.alt = data.title;
+                image.dataset.imageId = data.id;
+                image.dataset.editUrl = data.edit_url;
+                this.imageChooser.input.removeEventListener('change', changeEventFunc);
+            };
+
+            this.imageChooser.input.addEventListener('change', changeEventFunc);
+        });
+
+        const imageDelete = imageActions.addElement('button', {
+            className: 'wagtail-image-row-image-delete',
+            innerHTML: wagtailImageRowTrashCanIcon,
+        });
+
+        imageDelete.addEventListener('click', () => {
+            imageWrapper.remove();
+        });
+
+        imageWrapper.addEventListener('dblclick', () => {
+            imageActions.classList.toggle('wagtail-image-row-image-actions-visible');
+        });
     }
 
     render() {
