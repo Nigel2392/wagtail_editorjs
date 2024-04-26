5c6165f7907345bbfac7f4a0ba39e162f7c01aa0
Nigel van Keulen <nigel2392@gmail.com>
2024-04-27 01:56:28 +0200

    Update readme features; update github script; push pre-release

--- wagtail_editorjs/__init__.py
+++ wagtail_editorjs/__init__.py
@@ -7,5 +7,5 @@ Includes support for custom blocks, inline tools, and more.
 Everything is dynamic and customizable.
 """
 
-__version__ = '1.6.0'
+__version__ = '1.6.1rc1'
 VERSION = tuple([int(v) for v in __version__.split('.')])
